71106931de50c4e151ed4efd8be8e502dd0d1303
Константин Юрин <yurinkonstantin@mail.ru>
2018-12-17 07:37:31 +0300

    Добавьте файлы проекта.

---
+++ DataTableDataSetDataGrid/ClassTable.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace DataTableDataSetDataGrid
+{
+   public class ClassTable : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        DataTable _booksTable;
+        public DataTable booksTable
+        {
+            get
+            {
+                return _booksTable;
+            }
+            set
+            {
+                _booksTable = value;
+                this.OnPropertyChanged("booksTable");
+            }
+        }
+   
+        public DataGrid dataGrid = new DataGrid();
+        public void newTab(string name)
+        {
+          //  bookStore = new DataSet("BookStore");
+            booksTable = new DataTable("name");
+            
+            collection = new ObservableCollection<DataRow>();
+            // bookStore.Tables.Add(booksTable);
+            DataColumn idColumn = new DataColumn("Id", Type.GetType("System.Int32"));
+            idColumn.Unique = true; // столбец будет иметь уникальное значение
+            idColumn.AllowDBNull = false; // не может принимать null
+            idColumn.AutoIncrement = true; // будет автоинкрементироваться
+            idColumn.AutoIncrementSeed = 1; // начальное значение
+            idColumn.AutoIncrementStep = 1; // приращении при добавлении новой строки
+
+            DataColumn nameColumn = new DataColumn("Колонка 1", Type.GetType("System.Int32"));
+            DataColumn priceColumn = new DataColumn("Колонка 2", Type.GetType("System.Int32"));
+          
+            DataColumn discountColumn = new DataColumn("Колонка 3", Type.GetType("System.Int32"));
+         
+
+            booksTable.Columns.Add(idColumn);
+            booksTable.Columns.Add(nameColumn);
+            booksTable.Columns.Add(priceColumn);
+            booksTable.Columns.Add(discountColumn);
+            // определяем первичный ключ таблицы books
+            booksTable.PrimaryKey = new DataColumn[] { booksTable.Columns["Id"] };
+            DataRow row = booksTable.NewRow();
+            row.ItemArray = new object[] { null, 1, 200, 12 };
+           collection.Add(row); // добавляем первую строку
+            booksTable.Rows.Add(row);
+        
+         
+            // добавляем вторую строку
+        }
+
+        public string newColums()
+        {
+            if (booksTable != null) // table is a DataTable
+            {
+               int x =  booksTable.Columns.Count;
+                   DataColumn priceColumn = new DataColumn("Колонка "+x.ToString(), Type.GetType("System.Int32"));
+                priceColumn.AllowDBNull = true;
+                booksTable.Columns.Add(priceColumn);
+
+                return "Колонка " + x.ToString();
+
+            }
+            return null;
+        }
+        public ObservableCollection<DataRow> collection { get; set; }
+        public void newRows()
+        {
+           // row.ItemArray = new object[] { null, "Война и мир", 200 };
+            DataRow row = booksTable.NewRow();
+            int x = booksTable.Columns.Count-1;
+        
+            for (int i=0; i<x; i++)
+                {
+                    row[i+1] = "0";
+                }
+            // DataRow row1 = booksTable.NewRow();
+            
+            booksTable.Rows.Add(row);
+            collection.Add(row);
+            booksTable.AcceptChanges();
+       
+        
+
+            }
+    
+    }
+}
---
+++ DataTableDataSetDataGrid/MainPage.xaml.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.Toolkit.Uwp.UI.Controls;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Telerik.UI.Xaml.Controls.Grid;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Пустая страница" см. по адресу https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x419
+
+namespace DataTableDataSetDataGrid
+{
+    /// <summary>
+    /// Пустая страница, которую можно использовать саму по себе или для перехода внутри фрейма.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+      
+            
+     
+          
+
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _ClassTable = new ClassTable();
+            _ClassTable.newTab("fggg");
+         //  ass();
+            base.OnNavigatedTo(e);
+        }
+        public void ass()
+        {
+            if (_ClassTable.booksTable != null) // table is a DataTable
+            {
+                int i = 0;
+                foreach (DataColumn col in _ClassTable.booksTable.Columns)
+                {
+                // booksTable.Columns.Add(
+                //  new DataGridTextColumn
+                //  {
+                //    Header = col.ColumnName,
+
+                //   Binding = new Binding(string.Format("[{0}]", col.ColumnName))
+
+
+
+                //  });
+               
+                    DataGridTeleric.Columns.Add(new DataGridNumericalColumn()
+                    {
+                        Header = col.ColumnName,
+                      PropertyName= col.ColumnName.ToString()
+
+                    });
+                    dataGrid1.Columns.Add(new Microsoft.Toolkit.Uwp.UI.Controls.DataGridTextColumn()
+                    {
+                        Header = col.ColumnName,
+                        Binding = new Binding { Path = new PropertyPath("[" + col.ColumnName.ToString() + "]") },
+                        IsReadOnly = false
+                    });
+
+                }
+               
+            }
+        }
+       public ObservableCollection<DataRow> collection = new ObservableCollection<DataRow>();
+        DataSet bookStore;
+        DataTable booksTable;
+        ClassTable _ClassTable { get; set; }
+        TextBox _TextBox = new TextBox();
+        public void newColon(DataGrid dataGrid, string name)
+        {
+            dataGrid.Columns.Add(new Microsoft.Toolkit.Uwp.UI.Controls.DataGridTextColumn()
+            {
+                Header = name,
+                Binding = new Binding { Path = new PropertyPath("[" + name + "]") },
+                IsReadOnly = false
+            });
+          
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            newColon(_ClassTable.dataGrid, _ClassTable.newColums());
+            
+
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            _ClassTable.newRows();
+                         //   collection.Add(row);            // dataGrid1.ItemsSource = booksTable.DefaultView;
+
+
+
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string s = "g";
+            foreach (DataRow r in _ClassTable.booksTable.Rows)
+            {
+                foreach (var cell in r.ItemArray)
+                    s += cell.ToString() + "\n";// Console.Write("\t{0}", cell);
+               /// Console.WriteLine();
+            }
+            MessageDialog messageDialog = new MessageDialog(s);
+           await messageDialog.ShowAsync();
+        }
+
+        private void DataGrid1_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            //Create a new template column.
+            var templateColumn = new Microsoft.Toolkit.Uwp.UI.Controls.DataGridTemplateColumn();
+            Microsoft.Toolkit.Uwp.UI.Controls.DataGridTextColumn templateColumn1 = new Microsoft.Toolkit.Uwp.UI.Controls.DataGridTextColumn()
+            {
+                Header = "id",
+                Binding = new Binding { Path = new PropertyPath("[id]") },
+                IsReadOnly = false
+            };
+            DataGrid dataGrid = (DataGrid)sender;
+            dataGrid.Columns.Add(templateColumn1);
+           // templateColumn.Header = "Due Date";
+           // e.Column = templateColumn;
+        }
+      
+        private void DataGrid1_Loading(FrameworkElement sender, object args)
+        {
+          
+            DataGrid dataGrid = (DataGrid)sender;
+            _ClassTable.dataGrid = dataGrid;
+            if (_ClassTable.booksTable != null) // table is a DataTable
+            {
+                int i = 0;
+                foreach (DataColumn col in _ClassTable.booksTable.Columns)
+                {
+                    // booksTable.Columns.Add(
+                    //  new DataGridTextColumn
+                    //  {
+                    //    Header = col.ColumnName,
+
+                    //   Binding = new Binding(string.Format("[{0}]", col.ColumnName))
+
+
+
+                    //  });
+                  
+                    dataGrid.Columns.Add(new Microsoft.Toolkit.Uwp.UI.Controls.DataGridTextColumn()
+                    {
+                        Header = col.ColumnName,
+                        Binding = new Binding { Path = new PropertyPath("[" + col.ColumnName.ToString() + "]") },
+                        IsReadOnly = false
+                    });
+
+                }
+
+            }
+        }
+    }
+    
+}
